d9ea7f9dedb19854bfb532c5bbfd4628ed624d02
Shad Beard <shad.beard@ensequence.com>
2014-01-22 00:10:37 -0500

    Updating to use 0.0.2

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,14 +1,13 @@
 
-var eol = '\n';
+// var eol = '\n';
 
-var snippetText = [
-	"snippet log",
-	"	console.log(${1:})"
-].join(eol);
 
 var editor = ace.edit('editor');
 editor.setTheme('ace/theme/monokai');
 var session = editor.getSession();
 session.setMode('ace/mode/javascript');
 
-ace_snippets(editor, session, 'javascript', snippetText);
+// Concat together the built in extensions
+var myCustomSnippetText = JsExtensionSnippetText + angularSnippetText + ejsSnippetText;
+
+ace_snippets(editor, session, 'javascript', myCustomSnippetText);
