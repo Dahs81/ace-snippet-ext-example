32e27c49e410309d134577d384c041e56e2fdb3d
Shad Beard <shad.beard@ensequence.com>
2014-01-29 01:39:44 -0500

    Updated to use ace-snippet-extension 0.0.3

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -11,3 +11,14 @@ session.setMode('ace/mode/javascript');
 var myCustomSnippetText = JsExtensionSnippetText + angularSnippetText + ejsSnippetText;
 
 ace_snippets(editor, session, 'javascript', myCustomSnippetText);
+
+var names = getNames(editor, session, 'javascript', myCustomSnippetText);
+console.log('NAMES: ', names);
+
+var contentArr = getContent(editor, session, 'javascript', myCustomSnippetText);
+for(var i = 0; i < contentArr.length; i++) {
+	console.log(contentArr[i]);
+}
+
+// You could use the getName and getContent functions in a dropdown or table, etc
+// to display the list of available snippets that you have added.
